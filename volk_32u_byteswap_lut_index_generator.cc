83dd14caa7455b766614ede1d5e6d65ec6348898
Nathan West <nathan.west@okstate.edu>
2014-10-23 14:46:55 -0500

    add 64-bit and 16-bit values

--- volk_32u_byteswap_lut_index_generator.cc
+++ volk_32u_byteswap_lut_index_generator.cc
@@ -14,16 +14,21 @@ uint64_t byteswap_lut_index(uint8_t* chars)
 int main()
 {
 
-    uint8_t pattern01_chars[8] = {24, 16, 8, 0, 25, 17, 9, 1};
+    /* These patterns are a 64-bit byteswap */
+    //uint8_t pattern01_chars[8] = {25, 17, 9, 1, 24, 16, 8, 0};
+    //uint8_t pattern23_chars[8] = {27,19,11,3,26,18,10,2};
+    //uint8_t pattern45_chars[8] = {29,21,13,5,28,20,12,4};
+    //uint8_t pattern67_chars[8] = {31,23,15,7,30,22,14,6};
+
+    /* These patterns are a 16-bit byteswap */
+    uint8_t pattern01_chars[8] = {8,0,24,16,9,1,25,17};
+    uint8_t pattern23_chars[8] = {10,2,26,18,11,3,27,19};
+    uint8_t pattern45_chars[8] = {12,4,28,20,13,5,29,21};
+    uint8_t pattern67_chars[8] = {14,6,30,22,15,7,31,23};
+
     uint64_t pattern01 = byteswap_lut_index(pattern01_chars);
-
-    uint8_t pattern23_chars[8] = {26,18,10,2,27,19,11,3};
     uint64_t pattern23 = byteswap_lut_index(pattern23_chars);
-
-    uint8_t pattern45_chars[8] = {28,20,12,4,29,21,13,5};
     uint64_t pattern45 = byteswap_lut_index(pattern45_chars);
-
-    uint8_t pattern67_chars[8] = {30,22,14,6,31,23,15,7};
     uint64_t pattern67 = byteswap_lut_index(pattern67_chars);
 
     std::cout << "int_lookup01 = vcreate_u8(" << pattern01 << ");" << std::endl;
